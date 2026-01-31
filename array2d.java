a013a9e1d27c5ec1afd95df2d1e5fff087d1ca72
sumit kuamr <badsjsumitkumar@gmail.com>
2026-01-31 18:04:55 +0530

    learning 2d array concept

--- array2d.java
+++ array2d.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class array2d {
+import java.util.*;
+
+public class array2d {
+    public static void main(String [] args){
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter how many row you want : ");
+
+        int row = sc.nextInt();
+
+        System.out.println("Enter how many column you want");
+        int clumn = sc.nextInt();
+
+        System.out.println("Enter the value of row and column : ");
+
+        int [][] num = new int[row][clumn];
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<clumn; j++){
+                num [i][j]=sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<clumn; j++){
+                System.out.print(num[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+
+
+
+    }
 }
