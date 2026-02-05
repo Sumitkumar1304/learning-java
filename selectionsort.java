7d99ecfd53ea2371c1616e47f7cde813d2c031e5
sumit kuamr <badsjsumitkumar@gmail.com>
2026-02-05 22:06:31 +0530

    Performing selection sort on given array:

--- selectionsort.java
+++ selectionsort.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class selectionsort {
+import java.util.*;
+
+public class selectionsort{
+    public static void main(String args[]){
+        System.out.println("Perform selection sort on given array : ");
+
+        int arr[] = {4, 6, 2, 99, 67, 88, 45, 1, 5};
+
+        int n = arr.length;
+
+        for(int i = 0; i<n-1; i++){
+            int smallest = i;
+
+            for(int j = i + 1; j<n; j++){
+                if(arr[j] < arr[smallest]){
+                    smallest = j;
+                }
+            }
+            //swap
+            int temp = arr[smallest];
+            arr[smallest]= arr[i];
+            arr[i] = temp;
+        }
+
+        for(int x : arr){
+            System.out.print(x + " ");
+        }
+    }
 }
