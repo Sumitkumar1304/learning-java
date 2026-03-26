adb4ef2a083cc2a578eea7f6d6b6e277dc344f7f
Sumit kumar <92439120+Sumitkumar1304@users.noreply.github.com>
2026-03-26 14:16:18 +0530

    Add test message for Jenkins cron job verification

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String []args){
         System.out.println("Hello Programmers : \nSumit this side. ");
+        System.out.println("This is for testing that commit change is working on jenkins scheduling cron job");
     }
 }
